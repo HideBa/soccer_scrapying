a234d01a448c473a011a3689a2c0b32291f225da
Gen <babi.@gmail.com>
2019-10-13 00:21:23 +0900

    キャッシュのページからリンクを取得して2重にクローリングすることにする。いったんプッシュ

--- soccer_proj/soccer_proj/settings.py
+++ soccer_proj/soccer_proj/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'soccer_proj.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'soccer_proj (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+USER_AGENT = 'Chrome/67.0.3396.87 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -40,34 +40,34 @@ DOWNLOAD_DELAY = 3
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'soccer_proj.middlewares.SoccerProjSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'soccer_proj.middlewares.SoccerProjDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'soccer_proj.pipelines.SoccerProjPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -84,10 +84,10 @@ DOWNLOAD_DELAY = 3
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 DEPTH_LIMIT = 1
--- soccer_proj/soccer_proj/spiders/SoccerSpider.py
+++ soccer_proj/soccer_proj/spiders/SoccerSpider.py
@@ -2,10 +2,12 @@
 import scrapy
 from ..items import SoccerProjItem
 
+
 class SoccerspiderSpider(scrapy.Spider):
     name = 'SoccerSpider'
     allowed_domains = ['jfa.jp']
-    start_urls = ['http://www.jfa.jp/match/takamado_jfa_u18_premier2019/east/schedule_result/']
+    start_urls = [
+        'http://www.jfa.jp/match/takamado_jfa_u18_premier2019/east/schedule_result/']
 
     # def parse(self, response):
     #     print("start -----------")
@@ -14,11 +16,16 @@ class SoccerspiderSpider(scrapy.Spider):
     #         team_home = item.css('h3::text').extract_first()
     #         )
     #         print("-----------")
+    def parse_initialize(self, response):
+        start_path = response.css(
+            '#sub > div.subMenu > ul > li.local-west_schedule > a::attr(href)').extract_first()
+        return start_path
 
     def parse(self, response):
         # print('666666666666666666666')
         # page2 = response.urljoin(response.css('div.shcedule-tournament a::text')).extract_first()
         # print(page2)
+        path = self.parse_initialize(response)
         for topic in response.css('table.table_theme1'):
             item = SoccerProjItem()
             item['headline'] = topic.css('a::text').extract_first()
@@ -31,9 +38,6 @@ class SoccerspiderSpider(scrapy.Spider):
     def parse_detail(self, response):
         item = response.meta['item']
         item['url'] = response.url
-        item['team_home'] = response.css('div.score-board-header p::text').extract_first()
+        item['team_home'] = response.css(
+            'div.score-board-header p::text').extract_first()
         yield item
-
-
-
-    
