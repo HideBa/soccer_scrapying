b194613147f9296395cc4269444df38294344336
Gen <babi.@gmail.com>
2019-10-24 16:39:20 +0900

    U15をスクレイピング途中まで完了

--- soccer_proj/soccer_proj/spiders/SoccerSpider.py
+++ soccer_proj/soccer_proj/spiders/SoccerSpider.py
@@ -8,6 +8,8 @@ import re
 
 # yield SeleniumRequest(url=url, callback=self.parse_result)
 
+#  実行時CSVに書き出す場合はーーーーー　scrapy crawl SoccerSpider -o results/soccer.csv
+
 
 class SoccerspiderSpider(scrapy.Spider):
     name = 'SoccerSpider'
@@ -47,7 +49,7 @@ class SoccerspiderSpider(scrapy.Spider):
         # url = "http://www.jfa.or.jp/match/matches/2011/premier_league/east/match/index2.html"
         url = "http://www.jfa.or.jp/match/matches/2011/premier_league/west/match/index2.html"
 
-        # 以下2014年以降用
+        # 以下2014年以降用-------------------------------------------------------
         # # ここで、ブラウザを起動してページを開く
         # selenium_get(url)
         # # get_aで各試合の詳細URLのa要素を取得
@@ -57,8 +59,9 @@ class SoccerspiderSpider(scrapy.Spider):
         #     page = a.get_attribute('href')
         #     # それぞれのURLにおいてScrapyRequestを生成
         #     yield scrapy.Request(page, callback=self.parse)
+        # ------------------------------------------------------------------------------------
 
-        # 以下2013年以前用
+        # 以下2013年以前用-----------------------------------------
         # ここで、ブラウザを起動してページを開く
         selenium_get(url)
         # get_aで各試合の詳細URLのa要素を取得
@@ -68,9 +71,9 @@ class SoccerspiderSpider(scrapy.Spider):
         next = next[0]
         next_url = next.get_attribute('href')
         print("next_url ========== " + next_url)
-        # 2012、13年は以下を使用。
+        # 2012、13年は以下を使用。-------------------------
         # alist = get_a('div.mainarea div > table > tbody td:nth-child(6) a')
-        # 2011年はサイト構造が違うため以下を使用。
+        # 2011年はサイト構造が違うため以下を使用。--------------------------------------
         alist = get_a('div.mainarea div > table > tbody td:nth-child(5) a')
         # for文を回してそれぞれのhref属性を取得
         pages = []
@@ -83,10 +86,10 @@ class SoccerspiderSpider(scrapy.Spider):
             yield scrapy.Request(page, callback=self.parse2)
 
         selenium_get(next_url)
-        # 2012、13年の場合は以下を使用。
+        # 2012、13年の場合は以下を使用。----------------------------------------
         # next_alist = get_a(
         #     'div.mainarea div > table > tbody td:nth-child(6) a')
-        # 2011年のモノはサイト構造が違うため、以下を使用。
+        # 2011年のモノはサイト構造が違うため、以下を使用。--------------------------------
         next_alist = get_a(
             'div.mainarea div > table > tbody td:nth-child(5) a')
         next_pages = []
@@ -96,6 +99,49 @@ class SoccerspiderSpider(scrapy.Spider):
 
         for next_page in next_pages:
             yield scrapy.Request(next_page, callback=self.parse2)
+
+        # # 以下はU18の年度によってサイト構成が違うものを自動化する段階（途中）－－－－－－－－－－－－－－－－－－－－－－－－
+        # selenium_get(url)
+        # url = "http://www.jfa.jp/match/takamado_jfa_u18_premier2019/"
+        # domain = "http://www.jfa.jp"
+        # past_games_urls = self.get_past_games_url(url, domain)
+        # print("past_game-Url == == == ===" + str(past_games_urls))
+
+        # # get_aで各試合の詳細URLのa要素を取得
+        # next = get_a(
+        #     '#u18wrapper > div > div.mainarea > div > div > a:nth-child(2)')
+
+        # next = next[0]
+        # next_url = next.get_attribute('href')
+        # print("next_url ========== " + next_url)
+        # # 2012、13年は以下を使用。
+        # # alist = get_a('div.mainarea div > table > tbody td:nth-child(6) a')
+        # # 2011年はサイト構造が違うため以下を使用。
+        # alist = get_a('div.mainarea div > table > tbody td:nth-child(5) a')
+        # # for文を回してそれぞれのhref属性を取得
+        # pages = []
+        # for a in alist:
+        #     page = a.get_attribute('href')
+        #     pages.append(page)
+        #     print("page ============ " + page)
+
+        # for page in pages:
+        #     yield scrapy.Request(page, callback=self.parse2)
+
+        # selenium_get(next_url)
+        # # 2012、13年の場合は以下を使用。
+        # # next_alist = get_a(
+        # #     'div.mainarea div > table > tbody td:nth-child(6) a')
+        # # 2011年のモノはサイト構造が違うため、以下を使用。
+        # next_alist = get_a(
+        #     'div.mainarea div > table > tbody td:nth-child(5) a')
+        # next_pages = []
+        # for a in next_alist:
+        #     next_page = a.get_attribute('href')
+        #     next_pages.append(next_page)
+
+        # for next_page in next_pages:
+        #     yield scrapy.Request(next_page, callback=self.parse2)
 
     # def start_requests(self):
     #     # ここに取得したい試合結果一覧のURLをかく
@@ -125,21 +171,16 @@ class SoccerspiderSpider(scrapy.Spider):
     #             yield scrapy.Request(url, callback=self.parse)
 
     # def get_past_games_url(self, url, domain):
-    #     count = 0
     #     past_games = get_doms('#select-year2 > option')
     #     domain = domain
     #     past_games_url = []
     #     for a in past_games:
-    #         count += 1
-    #         if count < 3:
-    #             temp = a.get_attribute('value')
-    #             if 'http' in temp:
-    #                 past_game_url = temp
-    #             else:
-    #                 past_game_url = domain + temp
-    #             past_games_url.append(past_game_url)
+    #         temp = a.get_attribute('value')
+    #         if 'http' in temp:
+    #             past_game_url = temp
     #         else:
-    #             break
+    #             past_game_url = domain + temp
+    #         past_games_url.append(past_game_url)
     #     print('past_games_url==========' + str(past_games_url))
     #     return past_games_url  # 各年度のURL
 
@@ -175,83 +216,84 @@ class SoccerspiderSpider(scrapy.Spider):
     #         game_detail_url = a.get_attribute('href')
     #         game_detail_url_list.append(game_detail_url)
     #     return game_detail_url_list
+            # ここまでーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
-    # def parse(self, response):
-    #     print('response====================' + str(response))
-    #     item = SoccerProjItem()
-    #     item['leagu_name'] = response.css(
-    #         '#ttl_sp > img::attr(alt)').extract_first()
-    #     # いったんtempとして値を取得しているが、ここから後は正規表現だの、Splitみたいなので不要な文字列を削って出力すればよし
-    #     temp = response.css(
-    #         '#inner-header-score > div.text-schedule::text').extract_first()
-    #     item['team_home'] = response.css(
-    #         '#score-board-header > div:nth-child(1) > p:nth-child(2)::text').extract_first()
-    #     item['team_away'] = response.css(
-    #         '#score-board-header > div:nth-child(5) > p:nth-child(2)::text').extract_first()
-    #     item['url'] = response.url
-    #     results_home = response.css(
-    #         '#score-board-header > div:nth-child(2)::text').extract_first()
-    #     if results_home:
-    #         item['results_home'] = results_home
-    #     else:
-    #         item['results_home'] = 'None'
+    def parse(self, response):
+        print('response====================' + str(response))
+        item = SoccerProjItem()
+        item['leagu_name'] = response.css(
+            '#ttl_sp > img::attr(alt)').extract_first()
+        # いったんtempとして値を取得しているが、ここから後は正規表現だの、Splitみたいなので不要な文字列を削って出力すればよし
+        temp = response.css(
+            '#inner-header-score > div.text-schedule::text').extract_first()
+        item['team_home'] = response.css(
+            '#score-board-header > div:nth-child(1) > p:nth-child(2)::text').extract_first()
+        item['team_away'] = response.css(
+            '#score-board-header > div:nth-child(5) > p:nth-child(2)::text').extract_first()
+        item['url'] = response.url
+        results_home = response.css(
+            '#score-board-header > div:nth-child(2)::text').extract_first()
+        if results_home:
+            item['results_home'] = results_home
+        else:
+            item['results_home'] = 'None'
 
-    #     results_away = response.css(
-    #         '#score-board-header > div:nth-child(4)::text').extract_first()
-    #     if results_away:
-    #         item['results_away'] = results_away
-    #     else:
-    #         item['results_away'] = 'None'
+        results_away = response.css(
+            '#score-board-header > div:nth-child(4)::text').extract_first()
+        if results_away:
+            item['results_away'] = results_away
+        else:
+            item['results_away'] = 'None'
 
-    #     year_pattern = '20[0-9]{2}'
-    #     item['year'] = re.findall(year_pattern, temp)
-    #     # item['year'] = re.search(year_pattern, temp).group()
-    #     month_pattern = '[0-1][0-9]月'
-    #     item['month'] = re.findall(month_pattern, temp)
-    #     day_pattern = '[0-3][0-9]日'
-    #     item['day'] = re.findall(day_pattern, temp)
-    #     # 以下試合がまだ行われていないデータにはNoneを入れる
-    #     goal_home = response.css(
-    #         '#game-content-wrap > div.scorerLeft::text').extract()
-    #     if goal_home:
+        year_pattern = '20[0-9]{2}'
+        item['year'] = re.findall(year_pattern, temp)
+        # item['year'] = re.search(year_pattern, temp).group()
+        month_pattern = '[0-1][0-9]月'
+        item['month'] = re.findall(month_pattern, temp)
+        day_pattern = '[0-3][0-9]日'
+        item['day'] = re.findall(day_pattern, temp)
+        # 以下試合がまだ行われていないデータにはNoneを入れる
+        goal_home = response.css(
+            '#game-content-wrap > div.scorerLeft::text').extract()
+        if goal_home:
 
-    #         item['goal_home'] = goal_home
-    #     else:
-    #         item['goal_home'] = 'None'
+            item['goal_home'] = goal_home
+        else:
+            item['goal_home'] = 'None'
 
-    #     goal_away = response.css(
-    #         '#game-content-wrap > div.scorerRight::text').extract()
-    #     if goal_away:
-    #         item['goal_away'] = goal_away
-    #     else:
-    #         item['goal_away'] = 'None'
-    #     # time_temp = response.css('#game-content-wrap::text').extract()
-    #     # print("time_temp =========" + time_temp)
-    #     item['id'] = re.search('[0-9]{3,4}', temp).group()
-    #     if item['id'][-1] == 0:
-    #         item['round'] = re.search('第[0-9]+節', temp).group() + '10'
-    #     else:
-    #         item['round'] = re.search(
-    #             '第[0-9]+節', temp).group() + item['id'][-1]
+        goal_away = response.css(
+            '#game-content-wrap > div.scorerRight::text').extract()
+        if goal_away:
+            item['goal_away'] = goal_away
+        else:
+            item['goal_away'] = 'None'
+        # time_temp = response.css('#game-content-wrap::text').extract()
+        # print("time_temp =========" + time_temp)
+        item['id'] = re.search('[0-9]{3,4}', temp).group()
+        if item['id'][-1] == 0:
+            item['round'] = re.search('第[0-9]+節', temp).group() + '10'
+        else:
+            item['round'] = re.search(
+                '第[0-9]+節', temp).group() + item['id'][-1]
 
-    #     item['time'] = []
-    #     for elem in item['goal_home']:
-    #         temp = re.findall('.*分', elem)
-    #         if temp:
-    #             item['time'].append(temp)
-    #         else:
-    #             continue
+        item['time'] = []
+        for elem in item['goal_home']:
+            temp = re.findall('.*分', elem)
+            if temp:
+                item['time'].append(temp)
+            else:
+                continue
 
-    #     for elem in item['goal_away']:
-    #         temp = re.findall('.*分', elem)
-    #         if temp:
-    #             item['time'].append(temp)
-    #         else:
-    #             continue
+        for elem in item['goal_away']:
+            temp = re.findall('.*分', elem)
+            if temp:
+                item['time'].append(temp)
+            else:
+                continue
 
-    #     print('time=' + str(item['time']))
+        print('time=' + str(item['time']))
 
-    #     yield item
+        yield item
 
     def parse2(self, response):
         print('response====================' + str(response))
@@ -297,7 +339,6 @@ class SoccerspiderSpider(scrapy.Spider):
         item['day'] = re.findall(day_pattern, temp)
         # 以下試合がまだ行われていないデータにはNoneを入れる
         # ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-        # 現状以下に問題あり、おそらく要素取得できていない
         #u18wrapper > div > div.mainarea > div > table > tbody > tr > td.matchgoal1
         goal_home = response.css(
             'td.matchgoal1::text').extract()
@@ -352,42 +393,6 @@ class SoccerspiderSpider(scrapy.Spider):
 
     def closed(self, response):
         selenium_close()
-
-    # year = scrapy.Field()
-    # month = scrapy.Field()
-    # day = scrapy.Field()
-    # goal_home = scrapy.Field()
-    # goal_away = scrapy.Field()
-    # time = scrapy.Field()
-    # player = scrapy.Field()
-
-    #  CSVに書き出す場合はーーーーー　scrapy crawl SoccerSpider -o results/soccer.csv
-
-        # 違うサイトを参考にして見たミドルウェアの書き方（Chromeの場合）
-        # def start_requests(self):
-        #     url = "http://www.jfa.jp/match/takamado_jfa_u18_premier2019/east/schedule_result/"
-        #     selenium_get(url)
-        #     links = get_dom('')
-        #     yield scrapy.Request()
-
-        # def parse(self, response):
-        #     for table in response.css('#main-colum > div.section-block > div.shcedule-tournament.js-createSchedule > div:nth-child(1) > div.table-wrap-tournament > table'):
-        #         item = SoccerProjItem()
-        #         item['headline'] = table.xpath(
-        #             '//tbody/tr[1]/td[2]/text()').extract()
-        #         yield item
-
-        # -------------------------------------
-        # test
-        #     def parse(self, response):
-        #         for sample in response.css('html body.match.premier-lower.local-east_schedule div#container.outer-block.clearfix div#main-colum div.section-block div.shcedule-tournament.js-createSchedule'):
-        #             item = SoccerProjItem()
-        #             item['headline'] = sample.css('#sectionNo1::text').extracted()
-        #             yield item
-
-        #     def closed(self, reasion):
-        #         close_driver()
-        # ---------------------------------------------
 
         # def parse(self, response):
         #     for topic in response.css('#main-colum > div.section-block > div.shcedule-tournament.js-createSchedule > div:nth-child(1) > div.table-wrap-tournament > table > tbody > tr:nth-child(1) > td:nth-child(3) > div.tdWrap1 > ul > li.score > a::attr(href)'):
