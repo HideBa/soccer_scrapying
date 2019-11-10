aac9e71c4585aa227a7bb6ac4aafd63a2d2b8024
leopon55 <re.o.5123re.o@gmail.com>
2019-11-11 03:15:59 +0900

    データの追加や上書きができるようになりました（その時は、final用のやつはコメントアウトする仕様)

--- soccer_format/merging.py
+++ soccer_format/merging.py
@@ -11,6 +11,8 @@ import pandas as pd
 # In[2]:
 
 
+# finalの場合
+#フォルダ内の全ファイルをくっつける
 in_file = "./datatomerge"
 files = glob.glob(in_file+"/*.csv")
 frame = pd.DataFrame()
@@ -25,21 +27,36 @@ all_df.head()
 # In[3]:
 
 
+# # updの場合（2つを結合したいとき）
+# original = "./adj_soccer_2019_2014_10311138.csv"
+# org_df = pd.read_csv(original,encoding="UTF-8")
+# updater = "./adj_u18_2014_11080023.csv"
+# upd_df = pd.read_csv(updater,encoding="UTF-8")
+# # 上書きしたいとき（変更したい元ファイルのデータを削除）
+# org_df=org_df[org_df['year'] != int("2014")]
+
+# #結合
+# all_df = pd.concat([org_df,upd_df],sort=True,copy=False,ignore_index=True)
+
+
+# In[4]:
+
+
 all_df = pd.concat([all_df,all_df["time"].str.split("+",expand=True)],axis=1).drop("time", axis=1).fillna(-1)
 all_df.rename(columns={0: 'n_time', 1: 'a_time'}, inplace=True)
 all_df["n_time"] =all_df["n_time"].astype(int)
 all_df["a_time"] =all_df["a_time"].astype(int)
 
 
-# In[4]:
+# In[5]:
 
 
-all_df.sort_values(["year","leagu_name","id","n_time","a_time"],ascending=[True,False,True,True,True])
+all_df.sort_values(["year","leagu_name","id","n_time","a_time"],ascending=[True,False,True,True,True],inplace=True)
 all_df.reset_index(drop=True, inplace=True)
 all_df
 
 
-# In[5]:
+# In[6]:
 
 
 #additional timeをプラス表記に
@@ -47,22 +64,29 @@ all_df["time"] = all_df["n_time"]
 all_df.loc[ (all_df["a_time"] !=-1 ), "time"] = all_df["n_time"].astype(str) + str("+") + all_df["a_time"].astype(str)
 
 
-# In[6]:
+# In[7]:
 
 
 #列の整理
 all_df=all_df.loc[:,['id','leagu_name','year','month','day','round','team_home','team_away','url','results_away','results_home','goal_away','goal_home','time','player']]
 
 
-# In[7]:
+# In[8]:
 
 
 all_df
 
 
-# In[8]:
+# In[ ]:
 
 
+
+
+
+# In[9]:
+
+
+# finalの場合
 # タイムスタンプ
 import os
 from datetime import datetime
@@ -71,18 +95,26 @@ t = os.path.getmtime(in_file)
 dt = datetime.fromtimestamp(t)
 datadate = dt.strftime('%m%d%H%M')
 print (datadate)
-
-
-# In[9]:
-
-
-# CSV出力
-all_df.to_csv("./kakoufinal" + "_1_" + datadate + ".csv", 
+#CSV出力
+all_df.to_csv("./kakoufinal" + "_2_" + datadate + ".csv", 
           index=False   # インデックスを削除
          )
 
 
 # In[10]:
+
+
+# #updの場合
+# from pathlib import Path
+# newname = Path(original).stem
+
+# # 出力
+# all_df.to_csv("./upd1_" + newname + ".csv", 
+#           index=False   # インデックスを削除
+#          )
+
+
+# In[11]:
 
 
 # py出力
